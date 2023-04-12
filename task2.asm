5e60290c0ca742b89b79f28ef878b2bfff88bf14
Khoa Vũ <88573547+ToiTenLaKhoa@users.noreply.github.com>
2023-04-12 20:02:36 +0700

    Update task2.asm

--- task2.asm
+++ task2.asm
@@ -7,14 +7,14 @@ A:
     LONG(0xE3)      // A[2] – Address A+8 = A+4*2
     LONG(0x9)       // A[3] – Address A+12 = A+4*3
     LONG(0xA)       // …
-    LONG(0x1C)		  // A[5]
-    LONG(0x06)		  // A[6]
+    LONG(0x1C)		// A[5]
+    LONG(0x06)		// A[6]
     LONG(0x15)
 
 Start:
     i = 5 // For example i=5
 load:
-	  ADDC(r31, i , r0)
+	ADDC(r31, i , r0) //store i at R0
     MULC(r0, 4, r20)
     LD(r31, A + r20, r1)
     LD(r31, A + r20 + 4, r2)
